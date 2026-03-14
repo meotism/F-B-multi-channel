013366b179cc156f9e6cc79bbbdf9a0458f4f393
tuantn24 <tuantn24@fpt.com>
2026-03-14 22:36:29 +0700

    update version sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * This avoids fragile asset lists that break the SW install on any mismatch.
  */
 
-const CACHE_NAME = 'fb-restaurant-v20';
+const CACHE_NAME = 'fb-restaurant-v21';
 
 // ---------------------------------------------------------------------------
 // Install: activate immediately, no pre-caching
