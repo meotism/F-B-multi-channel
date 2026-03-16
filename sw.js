cd6ef8336d837b5a830d6b96138b8af8ac3085ad
tuantn24 <tuantn24@fpt.com>
2026-03-16 22:43:42 +0700

    fix responsive tablet

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * This avoids fragile asset lists that break the SW install on any mismatch.
  */
 
-const CACHE_NAME = 'fb-restaurant-v24';
+const CACHE_NAME = 'fb-restaurant-v25';
 
 // ---------------------------------------------------------------------------
 // Install: activate immediately, no pre-caching
